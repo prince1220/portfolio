265e7a656c281a7411249a2d31a3d5a35c3c89f0
prince1220 <frontdeskdesigns@gamil.com>
2024-09-17 00:08:19 -0400

    border project imgs. map array to project cards

--- src/Components/ProjectCarousel.js
+++ src/Components/ProjectCarousel.js
@@ -3,41 +3,13 @@ import React from 'react'
 export default function ProjectCarousel({images}) {
   return (
    <div   className="slider" >
-       <div className="image-container">
-          <img   src={images[0]} alt="Project One"/> 
-      </div>
+       {images.map(image=>{
+         return  <div className="image-container">
 
-      <div  className="image-container">
-      <img  src={images[1]} alt="Project One"/> 
-      </div>
+         <img   src={image} alt="Project One"/> 
+     </div>
+       })}
 
-      <div  className="image-container">
-         <img  src={images[2]} alt="Project One"/> 
-      </div>
-      <div  className="image-container">
-        <img  src={images[3]} alt="Project One"/> 
-      </div>
-      <div  className="image-container">
-      <img src={images[4]} alt="Project One"/> 
-      </div>
-      <div  className="image-container">
-       <img  src={images[5]} alt="Project One"/>
-      </div>
-      <div  className="image-container">
-      <img  src={images[6]} alt="Project One"/>
-      </div>
-      <div  className="image-container">
-      <img  src={images[7]} alt="Project One"/>
-      </div>
-      <div  className="image-container">
-      <img  src={images[8]} alt="Project One"/>
-      </div>
-      <div  className="image-container">
-      <img  src={images[9]} alt="Project One"/>
-      </div>
-      <div  className="image-container">
-      <img  src={images[10]} alt="Project One"/>
-      </div>
   </div>
   )
 }
