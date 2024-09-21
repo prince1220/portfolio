2ef98d147aa2a86d159fcd97647eeed1ad711db0
prince1220 <frontdeskdesigns@gamil.com>
2024-09-21 16:52:08 -0400

    Add portfolio page link to logo

--- src/Components/NavbarDesktop.js
+++ src/Components/NavbarDesktop.js
@@ -1,26 +1,26 @@
-import React from "react"//optional
+import React from "react"; //optional
 import { Link } from "react-router-dom";
-import NavbarLogo from "../Assets/img/logo.webp"
-import "./Navbar.css"
+import NavbarLogo from "../Assets/img/logo.webp";
+import "./Navbar.css";
 
-function NavbarDesktop(){
-
+function NavbarDesktop() {
   return (
-    <nav className="nav-content desktop" style={{height:100}}> {/*flex*/}
-      <div className="flexbox-logo"> 
-         <img className="frontdeskdesigns" src={NavbarLogo} />
+    <nav className="nav-content desktop" style={{ height: 100 }}> {/* flex */}
+      <div className="flexbox-logo">
+        {/* Wrap the logo image in a Link */}
+        <Link to="/portfolio">
+          <img className="frontdeskdesigns" src={NavbarLogo} alt="Navbar Logo" />
+        </Link>
       </div>
    
-      <ul className="nav-links"> {/*flex*/}
+      <ul className="nav-links"> {/* flex */}
         <li><Link to="/portfolio">PORTFOLIO</Link></li>
         <li><Link to="/bio">BIO</Link></li>
         <li><Link to="/contact">CONTACT</Link></li>
         <li><Link to="/printing">PRINTING</Link></li>
       </ul>
-   
-  </nav>
-  )
-
+    </nav>
+  );
 }
 
 export default NavbarDesktop;
